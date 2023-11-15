508dcc5f44c90e90f9f316ea471a9b8c5f5066f3
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 18:39:22 +0900

    feat: 주말 할인 이벤트 구현(주말인 경우 메인메뉴 할인)

--- src/main/java/christmas/model/event/WeekendDiscountEventPolicy.java
+++ src/main/java/christmas/model/event/WeekendDiscountEventPolicy.java
@@ -1,2 +1,31 @@
-package christmas.model.event;public class WeekendDiscountEventPolicy {
+package christmas.model.event;
+
+import christmas.model.menu.Menu;
+import christmas.model.menu.MenuType;
+import java.util.EnumMap;
+
+public class WeekendDiscountEventPolicy implements DiscountEventPolicy{
+
+    private static final int DISCOUNT_PER_MAIN = 2023;
+
+    @Override
+    public boolean isEventApplicable(int date) {
+        return DayType.from(date) == DayType.WEEKEND;
+    }
+
+    @Override
+    public int getDiscountAmount(int date, EnumMap<Menu, Integer> menus) {
+        if (isEventApplicable(date)) {
+            return calculateTotalDiscountForMains(menus);
+        }
+        return 0;
+    }
+
+    private int calculateTotalDiscountForMains(EnumMap<Menu, Integer> menus) {
+        return menus.entrySet().stream()
+            .filter(entry -> entry.getKey().getType() == MenuType.Main)
+            .mapToInt(entry -> entry.getValue() * DISCOUNT_PER_MAIN)
+            .sum();
+    }
+
 }
