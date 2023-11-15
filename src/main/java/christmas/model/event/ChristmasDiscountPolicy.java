952fab703ee73d45b522a7ff9918c8cdda6be6f8
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 14:54:07 +0900

    feat: 크리스마스 할인 이벤트 (날짜를 받아 그에 해당하는 할인값을 리턴) 구현

--- src/main/java/christmas/model/event/ChristmasDiscountPolicy.java
+++ src/main/java/christmas/model/event/ChristmasDiscountPolicy.java
@@ -1,2 +1,39 @@
-package christmas.model.event;public class ChristmasDiscountPolicy {
+package christmas.model.event;
+
+import christmas.validator.DateValidator;
+
+public class ChristmasDiscountPolicy implements DiscountPolicy {
+    private static final int START_DAY = 1;
+    private static final int CHRISTMAS_DAY = 25;
+    private static final int INITIAL_DISCOUNT = 1000;
+    private int discountAmount;
+    private int date;
+
+    public ChristmasDiscountPolicy(int date) {
+        this.discountAmount = INITIAL_DISCOUNT;
+        DateValidator.validateDate(date);
+        this.date = date;
+    }
+
+    @Override
+    public boolean isEventApplicable(int date) {
+        //이벤트 적용 여부를 파악
+        return START_DAY <= date && date <= CHRISTMAS_DAY;
+    }
+
+    public int calculateDiscountAmount(int totalOrderAmount) {
+        // 이벤트 적용 시
+        if (isEventApplicable(date)) {
+            int daysPassed = date - START_DAY; // 현재 날짜에서 시작일까지의 날짜 수 계산
+            int currentDiscount = INITIAL_DISCOUNT + daysPassed * 100; // 현재 할인 금액 계산
+            return Math.min(currentDiscount, totalOrderAmount);
+        }
+        // 이벤트가 적용되지 않을 때는 할인 없음을 반환
+        return 0;
+    }
+
+    @Override
+    public int getDiscountAmount() {
+        return discountAmount;
+    }
 }
---
+++ src/main/java/christmas/validator/DateValidator.java
@@ -0,0 +1,2 @@
+package christmas.validator;public class DateValidator {
+}
