b9316ec2a3b990e71d8fe8b04f1d11eaf8660c7e
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 18:38:14 +0900

    feat: 크리스마스 할인 이벤트 구현

--- src/main/java/christmas/model/event/ChristmasDiscountEventPolicy.java
+++ src/main/java/christmas/model/event/ChristmasDiscountEventPolicy.java
@@ -1,39 +1,28 @@
 package christmas.model.event;
 
+import christmas.model.menu.Menu;
 import christmas.validator.DateValidator;
+import java.util.EnumMap;
 
-public class ChristmasDiscountPolicy implements DiscountPolicy {
+public class ChristmasDiscountEventPolicy implements DiscountEventPolicy {
     private static final int START_DAY = 1;
     private static final int CHRISTMAS_DAY = 25;
     private static final int INITIAL_DISCOUNT = 1000;
-    private int discountAmount;
-    private int date;
+    private static final int DISCOUNT_INCREMENT_PER_DAY = 100;
 
-    public ChristmasDiscountPolicy(int date) {
-        this.discountAmount = INITIAL_DISCOUNT;
-        DateValidator.validateDate(date);
-        this.date = date;
-    }
 
     @Override
     public boolean isEventApplicable(int date) {
-        //이벤트 적용 여부를 파악
         return START_DAY <= date && date <= CHRISTMAS_DAY;
     }
 
-    public int calculateDiscountAmount(int totalOrderAmount) {
-        // 이벤트 적용 시
-        if (isEventApplicable(date)) {
-            int daysPassed = date - START_DAY; // 현재 날짜에서 시작일까지의 날짜 수 계산
-            int currentDiscount = INITIAL_DISCOUNT + daysPassed * 100; // 현재 할인 금액 계산
-            return Math.min(currentDiscount, totalOrderAmount);
-        }
-        // 이벤트가 적용되지 않을 때는 할인 없음을 반환
-        return 0;
-    }
-
     @Override
-    public int getDiscountAmount() {
-        return discountAmount;
+    public int getDiscountAmount(int date, EnumMap<Menu, Integer> menus) {
+        if (isEventApplicable(date)) {
+            int daysPassed = date - START_DAY;
+            int discountAmount = INITIAL_DISCOUNT + daysPassed * DISCOUNT_INCREMENT_PER_DAY;
+            return discountAmount;
+        }
+        return 0;
     }
 }
