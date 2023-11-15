17fd8546ffd18b32b4ef74fdfa8267c8114fc87b
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 22:34:51 +0900

    fix: 사용하지 않는 클래스 제거

--- src/main/java/christmas/validator/DateValidator.java
+++
@@ -1,9 +0,0 @@
-package christmas.validator;
-
-public class DateValidator {
-    public static void validateDate(int date) throws IllegalArgumentException {
-        if (date < 1 || date > 31) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
-    }
-}
