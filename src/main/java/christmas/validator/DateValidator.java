d6fd82783d2c2b69770f2eac1b8053c311b903f0
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 14:55:56 +0900

    feat: 입력 날짜가 유효한지 검증하는 기능 구현

--- src/main/java/christmas/validator/DateValidator.java
+++ src/main/java/christmas/validator/DateValidator.java
@@ -1,2 +1,9 @@
-package christmas.validator;public class DateValidator {
+package christmas.validator;
+
+public class DateValidator {
+    public static void validateDate(int date) throws IllegalArgumentException {
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
 }
