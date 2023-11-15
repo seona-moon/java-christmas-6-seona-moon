36540b56df818db5a5dc74bc8df366fd7d15036d
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 18:38:50 +0900

    feat: 스페셜 할인 이벤트 구현(총 주문금액에 따라 할인)

--- src/main/java/christmas/model/event/SpecialDiscountEventPolicy.java
+++ src/main/java/christmas/model/event/SpecialDiscountEventPolicy.java
@@ -1,2 +1,30 @@
-package christmas.model.event;public class SpecialDiscountEventPolicy {
+package christmas.model.event;
+
+import christmas.model.menu.Menu;
+import java.util.EnumMap;
+
+public class SpecialDiscountEventPolicy implements DiscountEventPolicy{
+
+    private static final int SPECIAL_DISCOUNT = 1000;
+
+    @Override
+    public boolean isEventApplicable(int date) {
+        return isSpecialDay(date);
+    }
+
+    @Override
+    public int getDiscountAmount(int date, EnumMap<Menu, Integer> menus) {
+        if (isEventApplicable(date)) {
+            return SPECIAL_DISCOUNT;
+        }
+        return 0;
+    }
+
+    private static boolean isSpecialDay(int date) {
+        int dayOfWeek = date % 7;
+        if (dayOfWeek==3 || date == 25) {
+            return true;
+        }
+        return false;
+    }
 }
