24ec8db9d56eb685ca2c535451a392402de4ad68
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 18:40:21 +0900

    feat: 평일과 주말을 구분하기 위한 enum 구현

--- src/main/java/christmas/model/event/DayType.java
+++ src/main/java/christmas/model/event/DayType.java
@@ -1,2 +1,24 @@
-package christmas.model.event;public enum DayType {
+package christmas.model.event;
+
+public enum DayType {
+    WEEKDAY,
+    WEEKEND;
+
+    public static DayType from(int date) {
+        int dayOfWeek = date % 7;
+        switch (dayOfWeek) {
+            case 1:
+            case 2:
+                return WEEKEND;
+            case 0:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                return WEEKDAY;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 }
+
