7cc17aaa1203734ff40d5ef41163d7e2a313cb07
seona-moon <fhflwhwl5@gmail.com>
2023-11-15 19:27:32 +0900

    feat: 주문 총계를 계산하는 유틸 구현

--- src/main/java/christmas/util/TotalAmountCalculator.java
+++ src/main/java/christmas/util/TotalAmountCalculator.java
@@ -1,2 +1,12 @@
-package christmas.util;public class TotalAmountCalculator {
+package christmas.util;
+
+import christmas.model.menu.Menu;
+import java.util.EnumMap;
+
+public class TotalAmountCalculator {
+    public static int calculateTotalAmount(EnumMap<Menu, Integer> menus) {
+        return menus.entrySet().stream()
+            .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
+            .sum();
+    }
 }
